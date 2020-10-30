2909244c132aa03b49e036d145b7f7497dc78646
zach <wz.horton@gmail.com>
2020-10-30 14:30:27 -0700

    Rstudio url correction

--- scripts/25-R-util.sh
+++ scripts/25-R-util.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt install r-base -y
-wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.3.1093-amd.deb
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.3.1093-amd64.deb
 sudo gdebi -n rstud*
 rm rstud*
